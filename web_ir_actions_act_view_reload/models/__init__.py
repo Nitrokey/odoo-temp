2a0e5f9cd9b81fd1dd720772857fc2017dee1064
jans23 <jan@suhr.org>
2023-01-12 18:29:12 +0100

    web_ir_actions_act_view_reload removed

--- web_ir_actions_act_view_reload/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import ir_actions_act_view_reload
--- web_ir_actions_act_view_reload/models/ir_actions_act_view_reload.py
+++
@@ -1,7 +0,0 @@
-from odoo import models
-
-
-class IrActionsActViewReload(models.Model):
-    _name = "ir.actions.act_view_reload"
-    _inherit = "ir.actions.actions"
-    _description = "View Reload"
